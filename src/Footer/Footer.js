2ef78f43aece5be1d9b8b244bf620b443770ad78
arctair <jtyler_@outlook.com>
2018-08-28 20:40:08 -0500

    remove cruft from Footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -6,11 +6,6 @@ import './Footer.css'
 const Footer = (props) => (
   <footer {...props}>
     <div>Streamlining storytelling</div>
-    <div>Contact Us</div>
-    <div className='contact'>
-      <a href='https://github.com/arctair/cruftbusters.arctair.com'><FaGithub /></a>
-      <a href='mailto:jtyler_@outlook.com'><FaEnvelope /></a>
-    </div>
     <div className='copyright'>
       © 2018 Cruftbusters. All Rights Reserved
     </div>
