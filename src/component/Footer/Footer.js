853f7f084ad63a035302d2f2a931c314033ddab7
arctair <jtyler_@outlook.com>
2018-09-18 23:28:17 -0500

    add social media

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaMedium, FaTwitter } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 import './Footer.css'
@@ -7,6 +8,16 @@ const Footer = (props) => (
   <footer {...props}>
     <div>Streamlining storytelling</div>
     <div><Link to='/about'>About Cruftbusters</Link></div>
+    <div className='social-media'>
+      <a href='https://medium.com/cruftbusters'>
+        <FaMedium aria-hidden />
+        <span className='sr-only'>Medium, Blog</span>
+      </a>
+      <a href='https://twitter.com/cruftbusters'>
+        <FaTwitter aria-hidden />
+        <span className='sr-only'>Twitter, Microblog</span>
+      </a>
+    </div>
     <div><a href='mailto:info@cruftbusters.com'>info@cruftbusters.com</a></div>
     <div className='copyright'>
       © 2018 Cruftbusters. All Rights Reserved
