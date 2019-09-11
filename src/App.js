aec0f6844d5621f25f0d674d2f65794a70c7205f
arctair <jtyler_@outlook.com>
2019-09-10 19:15:45 -0500

    KISS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import React from 'react';
+
+import {
+  Typography,
+} from '@material-ui/core';
+
 function App() {
-  return 'Hello world';
+  return (
+    <div>
+      <Typography variant='h2'>
+        Consulting
+      </Typography>
+      <ul>
+        <li>Web App Development</li>
+        <li>Web Service Development</li>
+        <li>Terminal/Console App Development</li>
+      </ul>
+      <Typography variant='h2'>
+        Workshops
+      </Typography>
+      <ul>
+        <li>XP teams/organizations</li>
+        <li>Test-Driven Development</li>
+        <li>Spring Boot</li>
+        <li>Apache Kafka</li>
+        <li>RabbitMQ</li>
+        <li>React</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
